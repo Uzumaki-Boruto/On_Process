b5b7a6eb6dd3fadb0fe711d7db8b3132765f87a6
Uzumaki Boruto <bbe.haminte@gmail.com>
2016-09-12 20:54:47 +0700

    WIP Taliyah

---
+++ UBTaliyah/Spells.cs
@@ -0,0 +1,32 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+
+
+namespace UBTaliyah
+{
+    class Spells
+    {
+        public static Spell.Skillshot Q { get; private set; }
+        public static Spell.Skillshot W { get; private set; }
+        public static Spell.Skillshot E { get; private set; }
+        public static Spell.Skillshot R { get; private set; }
+
+        public static void InitSpells()
+        {
+            Q = new Spell.Skillshot(SpellSlot.Q, 1000, SkillShotType.Linear, 250, 3600, 80);
+
+            W = new Spell.Skillshot(SpellSlot.W, 900, SkillShotType.Circular, 500, int.MaxValue, 50)
+            {
+                AllowedCollisionCount = int.MaxValue,
+            };
+
+            E = new Spell.Skillshot(SpellSlot.E, 800, SkillShotType.Cone, 250, 500, 40)
+            {
+                AllowedCollisionCount = int.MaxValue,
+            };
+
+            R = new Spell.Skillshot(SpellSlot.R, 3000, SkillShotType.Linear, 250, 2800, 110);
+        }
+    } 
+}
