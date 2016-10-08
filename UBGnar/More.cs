9d02caa539c1b901ef7d3a1770bff899fce5a1b0
Uzumaki Boruto <bbe.haminte@gmail.com>
2016-10-08 21:56:16 +0700

    Process Gnar

---
+++ UBGnar/More.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Rendering;
+using EloBuddy.SDK.Notifications;
+using SharpDX;
+
+namespace UBGnar
+{
+    class More
+    {
+        public static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.ChampionName != "Gnar") return;
+
+            Config.Dattenosa();
+            Spells.InitSpells();
+            InitEvents();
+        }
+        private static void InitEvents()
+        {
+            if (Config.DrawMenu.Checked("notif") && Config.DrawMenu.Checked("draw"))
+            {
+                var notStart = new SimpleNotification("UBGnar Load Status", "UBGnar sucessfully loaded.");
+                Notifications.Show(notStart, 5000);
+            }
+            Game.OnTick += GameOnTick;
+            Game.OnUpdate += Mode.AutoHarass;
+            Game.OnUpdate += Mode.Killsteal;
+
+            Obj_GeneralParticleEmitter.OnCreate += Mode.Obj_GeneralParticleEmitter_OnCreate;
+            Obj_GeneralParticleEmitter.OnDelete += Mode.Obj_GeneralParticleEmitter_OnDelete;
+            AIHeroClient.OnProcessSpellCast += Mode.Player_OnSpellCast;
+
+            Gapcloser.OnGapcloser += Mode.Gapcloser_OnGapcloser;
+            Interrupter.OnInterruptableSpell += Mode.Interrupter_OnInterruptableSpell;
+
+            Drawing.OnDraw += OnDraw;
+            Drawing.OnEndScene += Damage.Damage_Indicator;
+
+            Orbwalker.OnUnkillableMinion += Mode.On_Unkillable_Minion;
+
+        }
+             
+        private static void GameOnTick(EventArgs args)
+        {            
+            if (Player.Instance.IsDead) return;
+
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            { Mode.Combo(); }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            { Mode.Harass(); }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            { Mode.LaneClear(); }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+            { Mode.JungleClear(); }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Flee))
+            { Mode.Flee(); }
+
+        }        
+        private static void OnDraw(EventArgs args)
+        {
+            if (!Config.DrawMenu.Checked("draw")) return;
+            if (Config.DrawMenu.Checked("Qdr"))
+            {
+                Circle.Draw(Spells.QTiny.IsLearned ? Color.HotPink : Color.Zero, Spells.QTiny.Range, Player.Instance.Position);
+            }
+            if (Config.DrawMenu.Checked("Wdr"))
+            {
+                Circle.Draw(Spells.WMega.IsLearned ? Color.Yellow : Color.Zero, Spells.WMega.Range, Player.Instance.Position);
+            }
+            if (Config.DrawMenu.Checked("Edr"))
+            {
+                Circle.Draw(Spells.ETiny.IsLearned ? Color.AliceBlue : Color.Zero, Spells.ETiny.Range, Player.Instance.Position);
+            }
+            if (Config.DrawMenu.Checked("Rdr"))
+            {
+                Circle.Draw(Spells.R.IsLearned ? Color.Green : Color.Zero, Spells.R.Range, Player.Instance.Position);
+            }
+        }       
+    }
+}
---
+++ UBGnar/VectorHelp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+
+namespace UBGnar
+{
+    static class VectorHelp
+    {
+        public static List<Vector3> GetWallAroundMe(float Range ,float length, int quality = -1)
+        {
+            var PI2 = (float)Math.PI * 2;
+            var Wall = new List<Vector3>();
+            var position = Player.Instance.Position.To2D();
+            for (var radius = 1; radius <= Range; radius += 100)
+            {
+                if (quality == -1)
+                {
+                    quality = (int)(radius / 7 + 11);
+                }
+                var rad = new Vector2(0, radius);
+                for (var i = 0; i <= (int)(Math.Abs(quality * length / PI2)); i++)
+                {
+                    var Point = (position + rad).RotateAroundPoint(position, 0 + PI2 * i / quality * (length > 0 ? 1 : -1));
+                    if (Point.IsBuilding() || Point.IsWall())
+                    {
+                        Wall.Add(Point.To3DWorld());
+                    }
+                    else continue;
+                }
+                if (Wall.Count >= Config.Menu.GetValue("st"))
+                {
+                    break;
+                }
+            }
+            return Wall;                
+        }
+        public static Vector3 Parallel(this Vector2 start, Vector2 end, Vector2 source)
+        {
+            var line = new Geometry.Polygon.Line(start, end);
+            var line2 = line.MovePolygon(Player.Instance.Position.To2D());
+            return line2.Points.Last().To3DWorld();
+        }
+    }
+}
